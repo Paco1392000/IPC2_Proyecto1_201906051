8d1a6e12fa87b0c02df5523851224d5d65bd0ff9
Paco <silvapaco810@gmail.com>
2021-03-07 00:53:57 -0600

    V1.2

---
+++ P1/ListaSimple.py
@@ -0,0 +1,42 @@
+from Dato import Dato
+class ListaSimple:
+    def __init__(self):
+        self.inicio = None
+
+    #inserta el nuevo objeto
+    def insertar(self, name, x, y):
+        nuevo = Dato(name, x, y)
+        if self.inicio is None:
+            self.inicio = nuevo
+        else:
+            tmp = self.inicio
+            while tmp.siguiente is not None:
+                tmp = tmp.siguiente
+            tmp.siguiente = nuevo
+
+    #muestra el objeto
+    def mostrar(self):
+        tmp = self.inicio
+        contador = 1
+        while tmp is not None:
+            print(str(contador) + '. Nombre: ' + str(tmp.name) + '. x: ' + str(tmp.x) + '. y: ' + str(tmp.y) + '\n')
+            contador += 1
+            tmp = tmp.siguiente
+    
+    #Longitud o tamaño
+    def getSize(self):
+        tmp = self.inicio
+        cont = 0
+        while tmp is not None:
+            cont += 1
+            tmp = tmp.siguiente
+        return cont
+    
+    #busqueda y retorno
+    def getNodo(self, valor):
+        tmp = self.inicio
+        while tmp is not None:
+            if str.lower(tmp.name) == str.lower(valor):
+                return tmp
+            tmp = tmp.siguiente
+        return None
