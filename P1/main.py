13e7d862efde67b337381f02af5a037844a88a8e
Paco <silvapaco810@gmail.com>
2021-03-07 00:55:06 -0600

    V2

--- P1/main.py
+++ P1/main.py
@@ -1,6 +1,6 @@
 from tkinter import filedialog
-from Doble.ListaDoble import ListaDoble
-from Doble.ListaSimple import ListaSimple
+from ListaDoble import ListaDoble
+from ListaSimple import ListaSimple
 import xml.etree.cElementTree as ET
 
 #parsear xml para obtener clases de las cosas
@@ -8,21 +8,23 @@ tree = ET.parse('Matriz.xml')
 root = tree.getroot()
 
 #declarar listas
-ima = ListaSimple()
+ListaNueva = ListaSimple()
 
 #Recorrer lista de elementos XML
 for elemento in root:
-    for subelemento in elemento:
-        ima.insertar(subelemento.text, "")
-
+    if elemento.attrib['nombre'] != '':
+        for subelemento in elemento:
+            ListaNueva.insertar(subelemento.text, subelemento.attrib['x'], subelemento.attrib['y'])
+            
 listaClases = ListaDoble()
 #Clases = etree.xpath('//clase')
-for clase in root:
-    listaClases.insertar(clase.attrib['clase'], clase.attrib['n'], clase.attrib['m'])
+for matriz in root:
+    listaClases.insertar(matriz.attrib['nombre'], matriz.attrib['n'], matriz.attrib['m'])
 
 print('lista de clases de cosas sin cosas')
 listaClases.mostrar()
-clase_mam = listaClases.getNodo('ejempo')
-clase_mam.ima = ima
-print('\nLista de clases de algo')
+listamad = listaClases.getNodo('Ejemplo')
+listamad.algo = ListaNueva
+print('\nLista de Matriz')
 listaClases.mostrar()
+ListaNueva.mostrar()
